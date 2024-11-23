1b32d077af98cbb82fdb565a62e834063a0c45a5
Larissa Pissurno <larissapissurno@gmail.com>
2024-11-23 20:51:03 -0300

    create communities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_23_052212) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_23_234907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,5 +28,20 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_23_052212) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "communities", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.text "rules"
+    t.integer "total_members"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tawanies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "bio"
   end
 end
